0e7fead4919cc817ec2f8fe4e669b16e23a17bf9
klaus.moser <klaus.moser@fujitsu.com>
2022-11-07 11:07:28 +0100

    Add parametrizing test

--- grundlagen/mathematische_grundlagen/grundrechenarten.py
+++ grundlagen/mathematische_grundlagen/grundrechenarten.py
@@ -1,14 +1,18 @@
 
-def get_digit_num(number: int) -> int:
+def get_digit_num_of_decimal(number: int) -> int:
     """
     Get the number of digits of a number through using the modulo.
     :param number: Integer number.
     :return: Integer number of digits.
     """
+
+    if not isinstance(number, int):  # check type
+        raise Exception("Number is not integer/decimal")
+
     digits = 0
 
-    while number % 10 != 0:
-        number = int(number / 10)
+    while number > 0:
+        number = number // 10  # division without rest
         digits += 1
 
     return digits
--- test/test_grundrechenarten.py
+++ test/test_grundrechenarten.py
@@ -1,9 +1,9 @@
 import pytest
 from grundlagen.mathematische_grundlagen.grundrechenarten import *
 
-num = 1000
 
-print(num)
-digits = get_digit_num(number=num)
-assert digits == 4
+@pytest.mark.parametrize("test_input,expected", [(-1, 0), (1234, 4)])
+def test_grundrechenarten(test_input, expected):
 
+    digits = get_digit_num_of_decimal(number=test_input)
+    assert digits == expected

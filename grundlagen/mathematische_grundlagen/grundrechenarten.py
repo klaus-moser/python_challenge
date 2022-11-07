4081e3f3dfa23af4c0b1bf5e6c8cc8610c395f87
klaus.moser <klaus.moser@fujitsu.com>
2022-11-07 14:52:42 +0100

    get_digit_num Test + additional parameter

--- grundlagen/mathematische_grundlagen/grundrechenarten.py
+++ grundlagen/mathematische_grundlagen/grundrechenarten.py
@@ -1,4 +1,3 @@
-
 def get_digit_num_of_decimal(number: int) -> int:
     """
     Get the number of digits of a number through using the modulo.
@@ -13,6 +12,27 @@ def get_digit_num_of_decimal(number: int) -> int:
 
     while number > 0:
         number = number // 10  # division without rest
+        digits += 1
+
+    return digits
+
+
+def get_digit_num(number) -> int:
+    """
+    Returns the number of whole digits (before the
+    comma) of any number (int, float, etc.).
+    :param number: Any number.
+    :return: Integer number.
+    """
+
+    if isinstance(number, str):
+        raise TypeError("Only int, float allowed.")
+
+    digits = 0
+    rem_value = number
+
+    while rem_value >= 1:
+        rem_value, _ = divmod(rem_value, 10)
         digits += 1
 
     return digits
--- test/test_grundrechenarten.py
+++ test/test_grundrechenarten.py
@@ -3,13 +3,27 @@ from grundlagen.mathematische_grundlagen.grundrechenarten import *
 
 
 @pytest.mark.parametrize("test_input,expected", [(0, 0), (-42, 0), (42, 2)])
-def test_grundrechenarten_pass(test_input, expected):
+def test_get_digit_num_of_decimal_pass(test_input, expected):
     digits = get_digit_num_of_decimal(number=test_input)
     assert digits == expected
 
 
-@pytest.mark.parametrize("test_input,expected", [(-0.42, pytest.raises(TypeError)),
-                                                 (0.42, pytest.raises(TypeError))])
-def test_grundrechenarten_fail(test_input, expected):
+@pytest.mark.parametrize("test_input,expected", [(0.0, pytest.raises(TypeError)),
+                                                 (-0.42, pytest.raises(TypeError)),
+                                                 (0.42, pytest.raises(TypeError)),
+                                                 ("foobar", pytest.raises(TypeError))])
+def test_get_digit_num_of_decimal_fail(test_input, expected):
     with pytest.raises(Exception):
         assert get_digit_num_of_decimal(number=test_input)
+
+
+@pytest.mark.parametrize("test_input,expected", [(0.0, 0), (1, 1), (-0.42, 0),
+                                                 (0.42, 0), (42.42, 2)])
+def test_get_digit_num_pass(test_input, expected):
+    assert get_digit_num(number=test_input) == expected
+
+
+@pytest.mark.parametrize("test_input,expected", [("foobar", pytest.raises(TypeError))])
+def test_get_digit_num_fail(test_input, expected):
+    with pytest.raises(Exception):
+        assert get_digit_num(number=test_input) == expected
